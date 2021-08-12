db7768d48e417528b9f599618709881c073607be
thiagomart <thiagomsl@gmail.com>
2021-08-12 20:32:37 -0300

    chore(summary): update style from component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f0f2f5;
         --red: #E52E4D;
         --blue: #5429CC;
-        
+        --green: #33CC95;
         --blue-light: #6933FF;
         
         --text-title: #363f5f;
@@ -31,7 +31,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background: var(---background);
+        background: var(--background);
         -webkit-font-smoothing: antialiased;
     }
 
