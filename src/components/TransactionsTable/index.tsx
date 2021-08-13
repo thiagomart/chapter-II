86215546a9774d9e91a3417d987f753e2a6053a0
thiagomart <thiagomsl@gmail.com>
2021-08-12 21:03:03 -0300

    chore(project): Add TransactionsTable Component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de WebSite</td>
+                        <td className="deposit">R$ 12.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>01/10/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="whitdraw">- R$ 1.000,00</td>
+                        <td>Casa</td>
+                        <td>01/10/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>   
+    );
+
+}
