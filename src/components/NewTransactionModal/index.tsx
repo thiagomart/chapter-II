d895658842cb38af13261f7fcf3c5cdb021e44af
thiagomart <thiagomsl@gmail.com>
2021-08-17 17:55:56 -0300

    chore(newTransactionModal): Update style modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,44 @@
+import Modal from "react-modal";
+import { Container } from "./styles";
+import closeImg from '../../assets/close.svg';
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    return (
+        <Modal 
+            isOpen={isOpen} 
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close">
+                <img src={closeImg} alt="Fechar Modal" />
+            </button>
+            <Container>
+                <h2>Cadastrar Transação</h2>
+                <input 
+                    type="text"
+                    placeholder="Título"
+                />
+                <input 
+                    type="number"
+                    placeholder="Valor"
+                />
+                <input type="text" placeholder="categoria" />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+
+            </Container>
+      </Modal>  
+    )    
+}
