1ff29562939035cdb86d6cefeac6375b5261560a
thiagomart <thiagomsl@gmail.com>
2021-08-18 22:56:05 -0300

    chore(project): Create hook and refactory project

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactionsContext";
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -16,7 +16,7 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
 
-    const { createTransaction } = useContext(TransactionsContext);
+    const { createTransaction } = useTransactions();
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,14 +1,31 @@
-import { useContext } from 'react';
+import { useTransactions } from '../../hooks/useTransactionsContext';
+import { Container } from "./styles";
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext';
-import { Container } from "./styles";
 
 export function Summary() {
 
-    const { transactions } = useContext(TransactionsContext)
-    console.log(transactions);
+    const { transactions } = useTransactions();
+   
+    const summary = transactions.reduce((acc, transaction) => {
+        switch (transaction.type) {
+            case 'withdraw':
+                acc.withdraws += transaction.amount;
+                acc.total -= transaction.amount;
+                
+                break;
+            case 'deposit':
+                acc.deposits += transaction.amount;
+                acc.total += transaction.amount;
+                break;
+        }
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    })
 
     return (
         <Container>
@@ -17,21 +34,37 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong> R$1000,00 </strong>
+                <strong> 
+                    {new Intl.NumberFormat(Intl.DateTimeFormat().resolvedOptions().timeZoneName, {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits) }
+                 </strong>
             </div> 
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong>- R$500,00 </strong>
+                <strong>
+                    - 
+                    {new Intl.NumberFormat(Intl.DateTimeFormat().resolvedOptions().timeZoneName, {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdraws) }
+                </strong>
             </div> 
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong> R$500,00 </strong>
+                <strong>                
+                    {new Intl.NumberFormat(Intl.DateTimeFormat().resolvedOptions().timeZoneName, {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total) }
+                </strong>
             </div> 
         </Container> 
     );
